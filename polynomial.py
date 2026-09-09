c072784f3e7b76f39d32f16160dd0d1346a1d3e1
Bill Vu <billvu@bu.edu>
2026-09-09 17:21:11 -0400

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return Int(x)
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,11 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        poly1 = self.p1.evaluate(x)
+        poly2 = self.p2.evaluate(x)
+        return Int(poly1.i + poly2.i)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,7 +44,12 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        poly1 = self.p1.evaluate(x)
+        poly2 = self.p2.evaluate(x)
+        return Int(poly1.i * poly2.i)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
